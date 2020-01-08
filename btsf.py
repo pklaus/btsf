9b4c3f04363d0be1a5db45ff5cce97784ea0631a
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-01-08 13:49:56 +0100

    btsf main() printing first and last values

--- btsf.py
+++ btsf.py
@@ -182,8 +182,25 @@ class UnknownFileError(NameError):
 def main():
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument('-f', default=5, type=int, metavar='n', help="print first n entries")
+    parser.add_argument('-l', default=5, type=int, metavar='n', help="print last n entries")
     parser.add_argument('btsf_file')
     args = parser.parse_args()
     with BinaryTimeSeriesFile.openread(args.btsf_file) as f:
-        print(f"{args.btsf_file} - Number of entries: {f.n_entries()}")
-        print(f"First entry: {f.read_last()}")
+        print(f"{args.btsf_file} - Number of entries: {f.n_entries}")
+        print(f"Metrics:")
+        print('   '.join(f"{metric.identifier} ({metric.type})" for metric in f.metrics))
+        if args.f or args.l and f.n_entries:
+            if (args.f + args.l) < f.n_entries:
+                print(f"first {args.f} entries:")
+                for values in [f.read() for _ in range(args.f)]:
+                    print(f"{values}")
+                f.goto_entry(f.n_entries - args.l)
+                print("...")
+                print(f"last {args.l} entries:")
+                for values in [f.read() for _ in range(args.l)]:
+                    print(f"{values}")
+            else:
+                print("entries:")
+                for _ in range(f.n_entries):
+                    print(f"{f.read()}")
