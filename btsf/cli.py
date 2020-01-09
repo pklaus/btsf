cd12013d04b7be723486ec1e6ab60b301b06921a
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2020-01-09 15:15:32 +0100

    new CLI command: `btsf export file.btsf`

--- btsf/cli.py
+++ btsf/cli.py
@@ -22,6 +22,21 @@ def info(args):
                 for _ in range(f.n_entries):
                     print(f"{f.read()}")
 
+def export(args):
+    import sys
+    sys.stderr.write(f"Exporting {args.btsf_file} to {args.out_file.name}\n")
+    with BinaryTimeSeriesFile.openread(args.btsf_file) as f:
+        if args.format == 'csv':
+            args.out_file.write('; '.join(f"{metric.identifier}" for metric in f.metrics) + '\n')
+            for values in f.iter_entries():
+                args.out_file.write('; '.join(str(v) for v in values) + '\n')
+        elif args.format == 'tabular':
+            args.out_file.write(' '.join('{:20.20s}'.format(m.identifier) for m in f.metrics) + '\n')
+            for values in f.iter_entries():
+                args.out_file.write(' '.join('{:20.20}'.format(str(v)) for v in values) + '\n')
+        else:
+            raise NotImplementedError
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
@@ -32,6 +47,12 @@ def main():
     info_parser.add_argument('-l', default=5, type=int, metavar='n', help="print last n entries")
     info_parser.add_argument('btsf_file')
     info_parser.set_defaults(func=info)
+
+    export_parser = subparsers.add_parser('export')
+    export_parser.add_argument('--format', '-f', choices=('tabular', 'csv'), default='tabular')
+    export_parser.add_argument('btsf_file')
+    export_parser.add_argument('out_file', type=argparse.FileType('w'), default='-', nargs='?')
+    export_parser.set_defaults(func=export)
 
     args = parser.parse_args()
     args.func(args)
