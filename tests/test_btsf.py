cab225a2aad2a39239578ed3e2010d2c75a7b461
Philipp Klaus <philipp.l.klaus@web.de>
2020-01-09 16:18:29 +0100

    update tests to reflect recent API changes

--- tests/test_btsf.py
+++ tests/test_btsf.py
@@ -19,22 +19,22 @@ class TestBinaryTimeSeriesFile(TestCase):
         tuple_2 = (3.3, 4.4, 0xffffffffffffffff, 2)
         tuple_3 = (5.0, 1.0, 0x123456789ABCDEF1, 3)
         with BinaryTimeSeriesFile.create(tf.name, metrics) as f:
-            f.log_new_samples(*tuple_1)
-            f.log_new_samples(*tuple_2)
-            f.log_new_samples(*tuple_3)
+            f.append(*tuple_1)
+            f.append(*tuple_2)
+            f.append(*tuple_3)
 
         with BinaryTimeSeriesFile.openread(tf.name) as f:
             self.assertEqual(f._byte_order, '<')
             self.assertEqual(f._struct_size, 24)
             self.assertEqual(f._struct_format, '<dfQB3x')
             self.assertEqual(len(f._metrics), 4)
-            for wrote, got in zip(tuple_1, f.read()):
+            for wrote, got in zip(tuple_1, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_2, f.read()):
+            for wrote, got in zip(tuple_2, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_3, f.read()):
+            for wrote, got in zip(tuple_3, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            self.assertRaises(EOFError, f.read)
+            self.assertRaises(StopIteration, lambda: next(f))
 
 
         tuple_4 = (4.4, 44.4, 0x4444444444444444, 0xb4)
@@ -42,25 +42,25 @@ class TestBinaryTimeSeriesFile(TestCase):
         tuple_6 = (6.6, 66.6, 0x6666666666666666, 0xb6)
         with BinaryTimeSeriesFile.openwrite(tf.name) as f:
             self.assertEqual(f._metrics, metrics)
-            f.log_new_samples(*tuple_4)
-            f.log_new_samples(*tuple_5)
-            f.log_new_samples(*tuple_6)
+            f.append(*tuple_4)
+            f.append(*tuple_5)
+            f.append(*tuple_6)
 
         with BinaryTimeSeriesFile.openread(tf.name) as f:
             self.assertEqual(f._byte_order, '<')
             self.assertEqual(f._struct_size, 24)
             self.assertEqual(f._struct_format, '<dfQB3x')
             self.assertEqual(len(f._metrics), 4)
-            for wrote, got in zip(tuple_1, f.read()):
+            for wrote, got in zip(tuple_1, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_2, f.read()):
+            for wrote, got in zip(tuple_2, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_3, f.read()):
+            for wrote, got in zip(tuple_3, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_4, f.read()):
+            for wrote, got in zip(tuple_4, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_5, f.read()):
+            for wrote, got in zip(tuple_5, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            for wrote, got in zip(tuple_6, f.read()):
+            for wrote, got in zip(tuple_6, next(f)):
                 self.assertAlmostEqual(wrote, got, places=5)
-            self.assertRaises(EOFError, f.read)
+            self.assertRaises(StopIteration, lambda: next(f))
