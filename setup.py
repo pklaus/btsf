a56962374260d126c82c332a307555e31e089554
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2021-04-16 09:30:47 +0200

    ran `black --line-length 92 .`

--- setup.py
+++ setup.py
@@ -5,22 +5,26 @@ from setuptools import setup
 # read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-        LDESC = f.read()
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    LDESC = f.read()
 
 setup(
     name="btsf",
     version="1.0.dev0",
     description="btsf (for Binary Time Series File) is a package to store your data in a condensed and fast yet flexible way.",
     long_description=LDESC,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="Philipp Klaus",
     author_email="philipp.l.klaus@web.de",
     url="https://github.com/pklaus/btsf",
     license="GPL",
     packages=["btsf",],
     # py_modules = ['',],
-    entry_points={"console_scripts": ["btsf = btsf.cli:main",],},
+    entry_points={
+        "console_scripts": [
+            "btsf = btsf.cli:main",
+        ],
+    },
     zip_safe=True,
     platforms="any",
     extras_require={
